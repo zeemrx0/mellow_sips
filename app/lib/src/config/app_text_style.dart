76d497ad284c1eff73ffc4ec0e57e48c89090354
zeemrx0 <duyhq.zee@gmail.com>
2023-08-13 18:40:14 +0700

    Fix text styles

--- app/lib/src/config/app_text_style.dart
+++ app/lib/src/config/app_text_style.dart
@@ -3,23 +3,24 @@
 part of 'app_theme.dart';
 
 class _AppTextSize {
-  static const double T34 = 34;
-  static const double T30 = 30;
+  static const double T32 = 32;
+  static const double T28 = 28;
   static const double T24 = 24;
+
   static const double T20 = 20;
+  static const double T18 = 18;
   static const double T16 = 16;
   static const double T14 = 14;
-  static const double T12 = 12;
 }
 
 class _AppTextHeight {
-  static const double H34_44 = 44 / 34;
-  static const double H30_40 = 40 / 30;
-  static const double H24_32 = 32 / 24;
+  static const double H32_42 = 42 / 32;
+  static const double H28_38 = 38 / 28;
+  static const double H24_34 = 34 / 24;
   static const double H20_28 = 28 / 20;
+  static const double H18_26 = 26 / 18;
   static const double H16_24 = 24 / 16;
   static const double H14_22 = 22 / 14;
-  static const double H12_18 = 18 / 12;
 }
 
 abstract class _AppTextStyle {
@@ -34,81 +35,98 @@ abstract class _AppTextStyle {
   static TextTheme textTheme(AppColors appColor) {
     return TextTheme(
       // Heading 1
-      displayLarge: _textHeading1s.copyWith(
+      displayLarge: _textHeading1.copyWith(
         color: appColor.grayColor,
       ),
 
       // Heading 2
-      displayMedium: _textHeading2s.copyWith(
+      displayMedium: _textHeading2.copyWith(
         color: appColor.grayColor,
       ),
 
       // Heading 3
-      displaySmall: _textHeading3s.copyWith(
+      displaySmall: _textHeading3.copyWith(
         color: appColor.grayColor,
       ),
 
       // Heading 4
-      headlineLarge: _textHeading4m.copyWith(
+      headlineLarge: _textHeading4.copyWith(
         color: appColor.grayColor,
       ),
 
       // Heading 5
-      headlineMedium: _textHeading5m.copyWith(
+      headlineMedium: _textHeading5.copyWith(
         color: appColor.grayColor,
       ),
 
       // Heading 6
-      headlineSmall: _textHeading6m.copyWith(
+      headlineSmall: _textHeading6.copyWith(
         color: appColor.grayColor,
       ),
 
-      /// Body
-      bodyLarge: _textBody1r.copyWith(
+      // Body 1
+      bodyLarge: _textBody1.copyWith(
+        color: appColor.grayColor,
+      ),
+
+      // Body 2
+      bodyMedium: _textBody2.copyWith(
         color: appColor.grayColor,
       ),
     );
   }
 
-  static final TextStyle _textHeading1s = TextStyle(
-    height: _AppTextHeight.H34_44,
-    fontSize: _AppTextSize.T34,
+  static final TextStyle _textHeading1 = TextStyle(
+    height: _AppTextHeight.H32_42,
+    fontSize: _AppTextSize.T32,
     fontWeight: bold,
+    fontFamily: 'Work Sans',
   );
 
-  static final TextStyle _textHeading2s = TextStyle(
-    height: _AppTextHeight.H30_40,
-    fontSize: _AppTextSize.T30,
-    fontWeight: semiBold,
+  static final TextStyle _textHeading2 = TextStyle(
+    height: _AppTextHeight.H28_38,
+    fontSize: _AppTextSize.T28,
+    fontWeight: bold,
+    fontFamily: 'Work Sans',
   );
 
-  static final TextStyle _textHeading3s = TextStyle(
-    height: _AppTextHeight.H24_32,
+  static final TextStyle _textHeading3 = TextStyle(
+    height: _AppTextHeight.H24_34,
     fontSize: _AppTextSize.T24,
-    fontWeight: semiBold,
+    fontWeight: bold,
+    fontFamily: 'Work Sans',
   );
 
-  static final TextStyle _textHeading4m = TextStyle(
+  static final TextStyle _textHeading4 = TextStyle(
     height: _AppTextHeight.H20_28,
     fontSize: _AppTextSize.T20,
-    fontWeight: medium,
+    fontWeight: semiBold,
+    fontFamily: 'Work Sans',
   );
 
-  static final TextStyle _textHeading5m = TextStyle(
+  static final TextStyle _textHeading5 = TextStyle(
+    height: _AppTextHeight.H18_26,
+    fontSize: _AppTextSize.T18,
+    fontWeight: semiBold,
+    fontFamily: 'Work Sans',
+  );
+
+  static final TextStyle _textHeading6 = TextStyle(
     height: _AppTextHeight.H16_24,
     fontSize: _AppTextSize.T16,
-    fontWeight: medium,
+    fontWeight: semiBold,
+    fontFamily: 'Work Sans',
   );
 
-  static final TextStyle _textHeading6m = TextStyle(
+  static final TextStyle _textBody1 = TextStyle(
+    height: _AppTextHeight.H16_24,
+    fontSize: _AppTextSize.T16,
+    fontWeight: regular,
+  );
+
+  static final TextStyle _textBody2 = TextStyle(
     height: _AppTextHeight.H14_22,
     fontSize: _AppTextSize.T14,
-    fontWeight: medium,
-  );
-
-  static final TextStyle _textBody1r = TextStyle(
-    height: _AppTextHeight.H12_18,
-    fontSize: _AppTextSize.T12,
     fontWeight: regular,
   );
 }
--- app/lib/src/config/exts/app_text_style_ext.dart
+++ app/lib/src/config/exts/app_text_style_ext.dart
@@ -6,8 +6,32 @@ class AppTextStyleExt {
 
   static AppTextStyleExt get of => AppTextStyleExt._();
 
-  TextStyle? textBody1r = Get.textTheme.bodyLarge?.copyWith(
-    fontWeight: _AppTextStyle.regular,
+  TextStyle? textHeading1b = Get.textTheme.displayLarge?.copyWith(
+    fontWeight: _AppTextStyle.bold,
+  );
+
+  TextStyle? textHeading2b = Get.textTheme.displayMedium?.copyWith(
+    fontWeight: _AppTextStyle.bold,
+  );
+
+  TextStyle? textHeading3b = Get.textTheme.displaySmall?.copyWith(
+    fontWeight: _AppTextStyle.bold,
+  );
+
+  TextStyle? textHeading4s = Get.textTheme.headlineLarge?.copyWith(
+    fontWeight: _AppTextStyle.semiBold,
+  );
+
+  TextStyle? textHeading5s = Get.textTheme.headlineMedium?.copyWith(
+    fontWeight: _AppTextStyle.semiBold,
+  );
+
+  TextStyle? textHeading6s = Get.textTheme.headlineSmall?.copyWith(
+    fontWeight: _AppTextStyle.semiBold,
+  );
+
+  TextStyle? textBody1b = Get.textTheme.bodyLarge?.copyWith(
+    fontWeight: _AppTextStyle.bold,
   );
 
   TextStyle? textBody1s = Get.textTheme.bodyLarge?.copyWith(
@@ -18,13 +42,23 @@ class AppTextStyleExt {
     fontWeight: _AppTextStyle.medium,
   );
 
-  TextStyle? textBody1rUnderline = Get.textTheme.bodyLarge?.copyWith(
+  TextStyle? textBody1r = Get.textTheme.bodyLarge?.copyWith(
     fontWeight: _AppTextStyle.regular,
-    decoration: TextDecoration.underline,
   );
 
-  TextStyle? textBody1rItalic = Get.textTheme.bodyLarge?.copyWith(
+  TextStyle? textBody2b = Get.textTheme.bodyMedium?.copyWith(
+    fontWeight: _AppTextStyle.bold,
+  );
+
+  TextStyle? textBody2s = Get.textTheme.bodyMedium?.copyWith(
+    fontWeight: _AppTextStyle.semiBold,
+  );
+
+  TextStyle? textBody2m = Get.textTheme.bodyMedium?.copyWith(
+    fontWeight: _AppTextStyle.medium,
+  );
+
+  TextStyle? textBody2r = Get.textTheme.bodyMedium?.copyWith(
     fontWeight: _AppTextStyle.regular,
-    fontStyle: FontStyle.italic,
   );
 }
