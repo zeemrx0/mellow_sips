8d8241ea07831ead631c79185e1c6f7bb67f52a4
Duy Huỳnh Quốc <48668462+zeemrx0@users.noreply.github.com>
2023-08-21 14:53:02 +0700

    Feature/init skeleton (#2)
    * Move page to main components add Fix git ignore

--- app/lib/src/pages/demo/demo_controller.dart
+++ app/lib/src/pages/demo/demo_controller.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:app/src/components/main/appBar/app_bar_base_builder.dart';
-import 'package:app/src/components/page/app_main_page_base_builder.dart';
+import 'package:app/src/components/main/page/app_main_page_base_builder.dart';
 import 'package:app/src/config/app_theme.dart';
 import 'package:app/src/routes/app_pages.dart';
 import 'package:domain/domain.dart';
